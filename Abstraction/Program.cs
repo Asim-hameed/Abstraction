48ab9f8048a459d8a8bdd0c1ac7a05d1ce59975c
Asim-hameed <asimhameed31@gmail.com>
2023-04-03 18:13:21 +0500

    overRiding property to insure abstraction

--- Abstraction/Program.cs
+++ Abstraction/Program.cs
@@ -2,58 +2,105 @@
 {
     abstract class person
     {
+        public abstract uint id { get; set; }
+        public abstract string name { get; set; }
+
+
         // Abstract Methods that do not have body
         //public abstract void show();
 
-        public string fName;
-        public string lName;
-        public int age;
-        public string phoneNo;
-        public string _MyProperty;
-        public string MyProperty 
-        {
-            set 
-            {
-                this._MyProperty = value;
-            } 
-            get
-            {
-                return this._MyProperty;
-            }
-        }
-        public abstract void printDetails();
+        //public string fName;
+        //public string lName;
+        //public int age;
+        //public string phoneNo;
+        //public string _MyProperty;
+        //public string MyProperty 
+        //{
+        //    set 
+        //    {
+        //        this._MyProperty = value;
+        //    } 
+        //    get
+        //    {
+        //        return this._MyProperty;
+        //    }
+        //}
+        //public abstract void printDetails();
+
+
+        //Abstract Properties
+
+        
     }
     class student :  person
     {
-        public int rollNo;
-        public int fees;
-        
-        public override void printDetails()
-        {
-            string name = this.fName + " " + this.lName;
-            Console.WriteLine("Student Name is {0}",name);
-            Console.WriteLine("Studnet age is {0}",this.age);
-            Console.WriteLine("Studnet Phone No is {0}", this.phoneNo);
-            Console.WriteLine("Studnet Roll No is {0}", this.rollNo);
-            Console.WriteLine("Studnet Fees is {0}", this.fees);
-            Console.WriteLine(this.MyProperty);
+        uint stdid;
+        string stdname;
+        public override uint id 
+        { 
+            get
+            {
+                return this.stdid;
+            } 
+            set
+            {
+                if(value == 0)
+                {
+                    Console.WriteLine("Id Cannot be zero !!");
+                }
+                else
+                {
+                    this.stdid = value;
+                }
+            } 
         }
-    }
-    class teacher : person
-    {
-        public string qualificationl;
-        public int salary;
+        public override string name 
+        { 
+            get
+            {
+                return this.stdname;
+            } 
+            set
+            {
+                if(string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("Name cannot be null or empty !!");
+                }
+                else 
+                {
+                    this.stdname = value; 
+                }
+            } 
+        }
+        //public int rollNo;
+        //public int fees;
 
-        public override void printDetails()
-        {
-            string name = this.fName + " " + this.lName;
-            Console.WriteLine("Teacher Name is {0}", name);
-            Console.WriteLine("Teacher age is {0}", this.age);
-            Console.WriteLine("Teacher Phone No is {0}", this.phoneNo);
-            Console.WriteLine("Teacher Qualification No is {0}", this.qualificationl);
-            Console.WriteLine("Teacher Salary is {0}", this.salary);
-            Console.WriteLine(this.MyProperty);
-        }
+        //public override void printDetails()
+        //{
+        //    string name = this.fName + " " + this.lName;
+        //    Console.WriteLine("Student Name is {0}",name);
+        //    Console.WriteLine("Studnet age is {0}",this.age);
+        //    Console.WriteLine("Studnet Phone No is {0}", this.phoneNo);
+        //    Console.WriteLine("Studnet Roll No is {0}", this.rollNo);
+        //    Console.WriteLine("Studnet Fees is {0}", this.fees);
+        //    Console.WriteLine(this.MyProperty);
+        //}
+    }
+    class teacher //: person
+    {
+        //public string qualificationl;
+        //public int salary;
+
+        //public override void printDetails()
+        //{
+        //    string name = this.fName + " " + this.lName;
+        //    Console.WriteLine("Teacher Name is {0}", name);
+        //    Console.WriteLine("Teacher age is {0}", this.age);
+        //    Console.WriteLine("Teacher Phone No is {0}", this.phoneNo);
+        //    Console.WriteLine("Teacher Qualification No is {0}", this.qualificationl);
+        //    Console.WriteLine("Teacher Salary is {0}", this.salary);
+        //    Console.WriteLine(this.MyProperty);
+        //}
     }
 
 
@@ -105,30 +152,39 @@
             Console.WriteLine("Hello, World!");
 
 
+            //student student = new student();
+            //student.fName = "Asim";
+            //student.lName = "Hameed";
+            //student.rollNo = 0365;
+            //student.phoneNo = "03335121353";
+            //student.fees = 155000;
+            //student.age = 25;
+            //student.MyProperty = "Property";
+
+            //student.printDetails();
+
+            //Console.WriteLine("----------------------");
+
+            //teacher teacher = new teacher();
+            //teacher.fName = "Asif";
+            //teacher.lName = "Ameer";
+            //teacher.phoneNo = "03001234567";
+            //teacher.age = 35;
+            //teacher.salary = 90000;
+            //teacher.qualificationl = "CS,PhD";
+            //teacher.MyProperty = "Property";
+
+            //teacher.printDetails();
+
+
+            // Abstract Properties
+
             student student = new student();
-            student.fName = "Asim";
-            student.lName = "Hameed";
-            student.rollNo = 0365;
-            student.phoneNo = "03335121353";
-            student.fees = 155000;
-            student.age = 25;
-            student.MyProperty = "Property";
+            student.id = 1;
+            student.name = "";
+            Console.WriteLine(student.id);
 
-            student.printDetails();
-
-            Console.WriteLine("----------------------");
-
-            teacher teacher = new teacher();
-            teacher.fName = "Asif";
-            teacher.lName = "Ameer";
-            teacher.phoneNo = "03001234567";
-            teacher.age = 35;
-            teacher.salary = 90000;
-            teacher.qualificationl = "CS,PhD";
-            teacher.MyProperty = "Property";
-
-            teacher.printDetails();
-
+            Console.WriteLine(student.name);
         }
     }
 }
